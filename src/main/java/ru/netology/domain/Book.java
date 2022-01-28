c5a8764d2e4e56532e14444ddfe954c402c55696
Kalinin Yakov <z-chutvishechem@mail.ru>
2022-01-28 14:27:04 +0700

    next commit

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -2,8 +2,41 @@ package ru.netology.domain;
 
 import java.util.Objects;
 
-public class Book extends Product{
+public class Book extends Product {
     private String author;
+    private String nameOfBook;
+
+    public Book() {
+        super();
+    }
+
+    public Book(int id, String name, int price, String author, String nameOfBook) {
+        super(id, name, price);
+        this.author = author;
+        this.nameOfBook = nameOfBook;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", nameOfBook='" + nameOfBook + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) && Objects.equals(nameOfBook, book.nameOfBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), author, nameOfBook);
+    }
 
     public String getAuthor() {
         return author;
@@ -13,23 +46,11 @@ public class Book extends Product{
         this.author = author;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(author, book.author);
+    public String getNameOfBook() {
+        return nameOfBook;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(author);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "author='" + author + '\'' +
-                '}';
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
     }
 }
--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -4,6 +4,17 @@ import java.util.Objects;
 
 public class Smartphone extends Product {
     private String manufacturer;
+    private String nameOfSmartphone;
+
+    public Smartphone(int id, String name, int price, String manufacturer, String nameOfSmartphone) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+        this.nameOfSmartphone = nameOfSmartphone;
+    }
+    public Smartphone() {
+        super();
+
+    }
 
     public String getManufacturer() {
         return manufacturer;
@@ -13,23 +24,33 @@ public class Smartphone extends Product {
         this.manufacturer = manufacturer;
     }
 
+    public String getNameOfSmartphone() {
+        return nameOfSmartphone;
+    }
+
+    public void setNameOfSmartphone(String nameOfSmartphone) {
+        this.nameOfSmartphone = nameOfSmartphone;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         Smartphone that = (Smartphone) o;
-        return Objects.equals(manufacturer, that.manufacturer);
+        return Objects.equals(manufacturer, that.manufacturer) && Objects.equals(nameOfSmartphone, that.nameOfSmartphone);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(manufacturer);
+        return Objects.hash(super.hashCode(), manufacturer, nameOfSmartphone);
     }
 
     @Override
     public String toString() {
         return "Smartphone{" +
                 "manufacturer='" + manufacturer + '\'' +
+                ", nameOfSmartphone='" + nameOfSmartphone + '\'' +
                 '}';
     }
 }
